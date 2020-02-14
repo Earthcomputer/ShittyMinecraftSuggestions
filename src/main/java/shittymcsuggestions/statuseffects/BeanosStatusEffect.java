d26e7b6b41e19acf0bca9fced6a1caf9a70be9a9
Joe <burtonjae@hotmail.co.uk>
2020-02-14 23:53:15 +0000

    Rewrite flight to use a library

--- src/main/java/shittymcsuggestions/statuseffects/BeanosStatusEffect.java
+++ src/main/java/shittymcsuggestions/statuseffects/BeanosStatusEffect.java
@@ -1,12 +1,18 @@
 package shittymcsuggestions.statuseffects;
 
+import io.github.ladysnake.pal.AbilitySource;
+import io.github.ladysnake.pal.Pal;
+import io.github.ladysnake.pal.VanillaAbilities;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.AbstractEntityAttributeContainer;
 import net.minecraft.entity.effect.StatusEffectType;
 import net.minecraft.server.network.ServerPlayerEntity;
+import shittymcsuggestions.ShittyMinecraftSuggestions;
 import shittymcsuggestions.entity.ExplodingArrowEntity;
 
 public class BeanosStatusEffect extends ModStatusEffect {
+
+    private static final AbilitySource ABILITY_SOURCE = Pal.getAbilitySource(ShittyMinecraftSuggestions.MODID, "beanos");
 
     protected BeanosStatusEffect(StatusEffectType type, int color) {
         super(type, color);
@@ -16,9 +22,8 @@ public class BeanosStatusEffect extends ModStatusEffect {
     public void onApplied(LivingEntity entity, AbstractEntityAttributeContainer attributes, int amplifier) {
         if (entity instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) entity;
-            player.abilities.allowFlying = true;
-            player.abilities.flying = true;
-            player.sendAbilitiesUpdate();
+            Pal.grantAbility(player, VanillaAbilities.ALLOW_FLYING, ABILITY_SOURCE);
+            Pal.grantAbility(player, VanillaAbilities.FLYING, ABILITY_SOURCE);
         }
     }
 
@@ -26,8 +31,8 @@ public class BeanosStatusEffect extends ModStatusEffect {
     public void onRemoved(LivingEntity entity, AbstractEntityAttributeContainer attributes, int amplifier) {
         if (entity instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) entity;
-            player.interactionManager.getGameMode().setAbilitites(player.abilities);
-            player.sendAbilitiesUpdate();
+            Pal.revokeAbility(player, VanillaAbilities.ALLOW_FLYING, ABILITY_SOURCE);
+            Pal.revokeAbility(player, VanillaAbilities.FLYING, ABILITY_SOURCE);
         }
     }
 
