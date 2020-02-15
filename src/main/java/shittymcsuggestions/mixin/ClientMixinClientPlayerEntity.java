a290f3f82184b214658c04d5e3219934b342a6e0
Joe <burtonjae@hotmail.co.uk>
2020-02-15 17:48:18 +0000

    CaveBEE

--- src/main/java/shittymcsuggestions/mixin/ClientMixinClientPlayerEntity.java
+++ src/main/java/shittymcsuggestions/mixin/ClientMixinClientPlayerEntity.java
@@ -13,13 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import shittymcsuggestions.ModSounds;
 
 @Mixin(ClientPlayerEntity.class)
-public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
+public class ClientMixinClientPlayerEntity extends AbstractClientPlayerEntity {
 
-    private static final double CHINA_VELOCITY = 0.25;
+    @Unique private static final double CHINA_VELOCITY = 0.25;
     @Unique private boolean hasReachedChinaVelocity;
     @Unique private int chinaBits;
+    @Unique private int ticksSinceUpwardVelocity = 0;
+    @Unique private boolean hasPlayedCavebee = true; // disable on first login
 
-    public MixinClientPlayerEntity(ClientWorld world, GameProfile profile) {
+    public ClientMixinClientPlayerEntity(ClientWorld world, GameProfile profile) {
         super(world, profile);
     }
 
@@ -28,16 +30,28 @@ public class MixinClientPlayerEntity extends AbstractClientPlayerEntity {
         chinaBits <<= 1;
         if (getVelocity().y <= -CHINA_VELOCITY)
             chinaBits |= 1;
-        // the number of ticks out of the last 32 ticks that we were at the criticial china velocity
+        // the number of ticks out of the last 32 ticks that we were at the critical china velocity
         int chinaTicks = Integer.bitCount(chinaBits);
+        ticksSinceUpwardVelocity++;
+        if (getVelocity().y > 0)
+            ticksSinceUpwardVelocity = 0;
 
-        if (chinaTicks >= 11) {
+        if (chinaTicks >= 11 && ticksSinceUpwardVelocity > 30) {
             if (!hasReachedChinaVelocity) {
                 hasReachedChinaVelocity = true;
                 world.playSoundFromEntity(this, this, ModSounds.CHINA, SoundCategory.PLAYERS, 1, 1);
             }
         } else if (chinaTicks < 8) {
             hasReachedChinaVelocity = false;
+        }
+
+        if (getY() <= 20) {
+            if (!hasPlayedCavebee) {
+                hasPlayedCavebee = true;
+                world.playSoundFromEntity(this, this, ModSounds.CAVEBEE, SoundCategory.PLAYERS, 1, 1);
+            }
+        } else if (getY() > 22) {
+            hasPlayedCavebee = false;
         }
     }
 
