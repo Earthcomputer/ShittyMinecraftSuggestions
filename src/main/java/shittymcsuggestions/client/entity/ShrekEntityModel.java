beccc5d3734a633d917cea3a1e551f54cbd3df33
Joe <burtonjae@hotmail.co.uk>
2020-04-25 22:20:46 +0100

    Mess up shrek

--- src/main/java/shittymcsuggestions/client/entity/ShrekEntityModel.java
+++ src/main/java/shittymcsuggestions/client/entity/ShrekEntityModel.java
@@ -1,45 +1,61 @@
 package shittymcsuggestions.client.entity;
 
 import net.minecraft.client.model.ModelPart;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import shittymcsuggestions.entity.ShrekEntity;
 
-public class ShrekEntityModel extends EntityModel<ShrekEntity> {
-
-    private final ModelPart bb_main;
+public class ShrekEntityModel extends BipedEntityModel<ShrekEntity> {
 
     public ShrekEntityModel() {
-        textureWidth = 128;
-        textureHeight = 128;
+        super(0, -60, 128, 128);
 
-        bb_main = new ModelPart(this);
-        bb_main.setPivot(0.0F, 24.0F, 0.0F);
-        bb_main.setTextureOffset(0, 57).addCuboid(-9.0F, -22.0F, -1.0F, 6, 22, 6, 0.0F);
-        bb_main.setTextureOffset(0, 0).addCuboid(-10.0F, -44.0F, -5.0F, 20, 22, 13, 0.0F);
-        bb_main.setTextureOffset(64, 33).addCuboid(-8.0F, -36.0F, -7.0F, 16, 12, 2, 0.0F);
-        bb_main.setTextureOffset(0, 35).addCuboid(-6.0F, -55.0F, -4.0F, 12, 11, 11, 0.0F);
-        bb_main.setTextureOffset(46, 46).addCuboid(3.0F, -22.0F, -1.0F, 6, 22, 6, 0.0F);
-        bb_main.setTextureOffset(66, 0).addCuboid(12.0F, -43.0F, -1.0F, 5, 22, 5, 0.0F);
-        bb_main.setTextureOffset(24, 59).addCuboid(-17.0F, -43.0F, -1.0F, 5, 22, 5, 0.0F);
-        bb_main.setTextureOffset(58, 75).addCuboid(-12.0F, -43.0F, -1.0F, 2, 5, 5, 0.0F);
-        bb_main.setTextureOffset(0, 5).addCuboid(-8.0F, -53.0F, 1.0F, 2, 3, 2, 0.0F);
-        bb_main.setTextureOffset(0, 5).addCuboid(6.0F, -53.0F, 1.0F, 2, 3, 2, 0.0F);
-        bb_main.setTextureOffset(70, 55).addCuboid(-12.0F, -56.0F, 0.0F, 4, 4, 4, 0.0F);
-        bb_main.setTextureOffset(70, 47).addCuboid(8.0F, -56.0F, 0.0F, 4, 4, 4, 0.0F);
-        bb_main.setTextureOffset(44, 74).addCuboid(10.0F, -43.0F, -1.0F, 2, 5, 5, 0.0F);
-        bb_main.setTextureOffset(0, 3).addCuboid(-1.0F, -50.0F, -5.0F, 2, 2, 2, 0.0F);
-    }
+        leftLeg = new ModelPart(this);
+        // left leg
+        leftLeg.setTextureOffset(0, 57).addCuboid(-9.0F, -22.0F, -1.0F, 6, 22, 6, 0.0F);
 
-    @Override
-    public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
-        bb_main.render(matrices, vertexConsumer, light, overlay, red, green, blue, alpha);
+        torso = new ModelPart(this);
+        // main torso
+        torso.setTextureOffset(0, 0).addCuboid(-10.0F, -44.0F, -5.0F, 20, 22, 13, 0.0F);
+        // belly
+        torso.setTextureOffset(64, 33).addCuboid(-8.0F, -36.0F, -7.0F, 16, 12, 2, 0.0F);
+
+        head = new ModelPart(this);
+        head.setPivot(0, -60, 0);
+        // main head
+        head.setTextureOffset(0, 35).addCuboid(-6.0F, -55.0F, -4.0F, 12, 11, 11, 0.0F);
+        // nose
+        head.setTextureOffset(0, 3).addCuboid(-1.0F, -50.0F, -5.0F, 2, 2, 2, 0.0F);
+
+        rightLeg = new ModelPart(this);
+        // right leg
+        rightLeg.setTextureOffset(46, 46).addCuboid(3.0F, -22.0F, -1.0F, 6, 22, 6, 0.0F);
+
+        rightArm = new ModelPart(this);
+        // right arm
+        rightArm.setTextureOffset(66, 0).addCuboid(12.0F, -43.0F, -1.0F, 5, 22, 5, 0.0F);
+        // right armpit
+        rightArm.setTextureOffset(44, 74).addCuboid(10.0F, -43.0F, -1.0F, 2, 5, 5, 0.0F);
+
+        leftArm = new ModelPart(this);
+        // left arm
+        leftArm.setTextureOffset(24, 59).addCuboid(-17.0F, -43.0F, -1.0F, 5, 22, 5, 0.0F);
+        // left armpit
+        leftArm.setTextureOffset(58, 75).addCuboid(-12.0F, -43.0F, -1.0F, 2, 5, 5, 0.0F);
+
+        helmet = new ModelPart(this);
+        // left inner ear
+        helmet.setTextureOffset(0, 5).addCuboid(-8.0F, -53.0F, 1.0F, 2, 3, 2, 0.0F);
+        // right inner ear
+        helmet.setTextureOffset(0, 5).addCuboid(6.0F, -53.0F, 1.0F, 2, 3, 2, 0.0F);
+        // left outer ear
+        helmet.setTextureOffset(70, 55).addCuboid(-12.0F, -56.0F, 0.0F, 4, 4, 4, 0.0F);
+        // right outer ear
+        helmet.setTextureOffset(70, 47).addCuboid(8.0F, -56.0F, 0.0F, 4, 4, 4, 0.0F);
     }
 
     @Override
     public void setAngles(ShrekEntity entity, float breathingProgress, float armPitchScale, float armRollScale, float yaw, float pitch) {
-        bb_main.yaw = yaw * (float)Math.PI / 180;
+        super.setAngles(entity, breathingProgress, armPitchScale, armRollScale, yaw, pitch);
     }
 
 }
--- src/main/java/shittymcsuggestions/entity/ShrekEntity.java
+++ src/main/java/shittymcsuggestions/entity/ShrekEntity.java
@@ -1,11 +1,19 @@
 package shittymcsuggestions.entity;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.WanderAroundGoal;
+import net.minecraft.entity.mob.MobEntityWithAi;
 import net.minecraft.world.World;
 
-public class ShrekEntity extends MobEntity {
+public class ShrekEntity extends MobEntityWithAi {
     protected ShrekEntity(EntityType<? extends ShrekEntity> type, World world) {
         super(type, world);
+    }
+
+    @Override
+    protected void initGoals() {
+        goalSelector.add(7, new WanderAroundGoal(this, 1));
+        goalSelector.add(8, new LookAroundGoal(this));
     }
 }
